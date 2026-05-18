ca98d19de2596b16958fc0821441f5aa6ee50c30
Upama <upamapokharel61@gmail.com>
2026-05-18 17:20:46 +0545

    Added error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,65 @@
-// import { Show, SignInButton, UserButton } from "@clerk/react";
+import { useEffect } from "react";
+
+import { Route, Routes, Navigate } from "react-router-dom";
+
+import { Show, SignIn } from "@clerk/react";
+
+import { useDispatch } from "react-redux";
 import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home/Home";
 
+import MoodBoard from "./pages/MoodBoard";
+
+import AuthCallback from "./pages/AuthCallback";
+
+import { ErrorBoundary } from "react-error-boundary";
+
+import { getCollectionPhotos } from "./api/unsplash";
+
+import { setLikedPhotos } from "./Features/Moodboard/moodboardSlice";
+import { useAppSelector } from "./app/hooks";
+
 function App() {
+  const dispatch = useDispatch();
+
+  const accessToken = useAppSelector((state) => state.unsplashAuth.accessToken);
+
+  useEffect(() => {
+    const syncCollection = async () => {
+      if (accessToken) {
+        const collectionPhotos = await getCollectionPhotos(accessToken);
+
+        dispatch(setLikedPhotos(collectionPhotos));
+      }
+    };
+
+    syncCollection();
+  }, [accessToken, dispatch]);
+
   return (
     <>
-      <Navbar />
-       <Home />
+      <ErrorBoundary fallback={<div>Something Went wrong</div>}>
+        <Show when="signed-out">
+          <div className="d-flex justify-content-center align-items-center vh-100">
+            <SignIn forceRedirectUrl="/home" />
+          </div>
+        </Show>
 
+        <Show when="signed-in">
+          <Navbar />
+
+          <Routes>
+            <Route path="/home" element={<Home />} />
+
+            <Route path="/moodboard" element={<MoodBoard />} />
+
+            <Route path="/auth/callback" element={<AuthCallback />} />
+
+            <Route path="*" element={<Navigate to="/home" />} />
+          </Routes>
+        </Show>
+      </ErrorBoundary>
     </>
   );
 }
